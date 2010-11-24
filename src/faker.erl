64bb3a66ae7e29792b01ee062e935bf824daa4c5
Mike Dvorkin <mike@dvorkin.net>
2010-11-23 22:56:45 -0800

    Implemented faker:sentence and faker:sentences

--- src/faker.erl
+++ src/faker.erl
@@ -6,7 +6,7 @@
 
 -module(faker).
 -export([name/0, first_name/0, last_name/0, prefix/0, suffix/0]).
--export([words/0, words/1]).
+-export([words/0, words/1, sentence/0, sentence/1, sentences/0, sentences/1]).
 
 -include("names.hrl").
 -include("lorem.hrl").
@@ -54,6 +54,25 @@ words(Count, Acc) ->
         _Less -> words(Count, [sample(?WORDS) | Acc])
     end.
 
+sentence() ->
+    sentence(4).
+
+sentence(MinWordCount) ->
+    Words = words(MinWordCount + random:uniform(7)),
+    Sentence = string:join([atom_to_list(W) || W <- Words], " "),
+    [string:to_upper(hd(Sentence)) | tl(Sentence)] ++ ".".
+
+sentences() ->
+    sentences(3).
+
+sentences(MinSentenceCount) ->
+    sentences(MinSentenceCount, []).
+
+sentences(MinSentenceCount, Acc) ->
+    case length(Acc) of
+        MinSentenceCount -> Acc;
+        _Less -> sentences(MinSentenceCount, [sentence() | Acc])
+    end.
 
 % Private methods.
 %----------------------------------------------------------------------------
